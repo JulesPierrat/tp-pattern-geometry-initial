601085b5522b6db49dc74741654a87c6023b9f8e
JulesPierrat <jules.pierrat@ensg.eu>
2022-01-03 17:40:43 +0100

    0.8

---
+++ src/main/java/org/acme/geometry/WktWriter.java
@@ -0,0 +1,38 @@
+package org.acme.geometry;
+
+public class WktWriter {
+    // Attribute
+
+    // Constructor
+    public WktWriter(){
+
+    }
+    
+    // Method
+    public String write(Geometry geometry){
+        // case of a point
+        if (geometry.getType() == "Point"){
+            Point point = (Point)geometry;
+            if (point.isEmpty()) {
+                return "POINT EMPTY";
+            }
+            return "POINT("+ point.getCoordinate().getX() +" "+point.getCoordinate().getY()+")";
+        } else if (geometry.getType() == "LineString") {
+            LineString line = (LineString)geometry;
+            if (line.isEmpty()) {
+                return "LINESTRING EMPTY";
+            }
+            String r = "LINESTRING(";
+            for(int i = 0 ; i < line.getNumPoints() ; i++){
+                r += line.getPointN(i).getCoordinate().getX() + " " + line.getPointN(i).getCoordinate().getY();
+                if (i != line.getNumPoints() - 2){
+                    r += ",";
+                }
+                r += ")";
+            }
+            return r;
+        } else {
+            throw new RuntimeException("geometry type not supported");
+        }
+    }
+}

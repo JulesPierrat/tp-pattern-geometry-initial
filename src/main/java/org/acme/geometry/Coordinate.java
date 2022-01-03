cc28ea3658f5965140d5de260ffcf4bf8f88d4f5
JulesPierrat <jules.pierrat@ensg.eu>
2022-01-03 13:55:48 +0100

    0.1

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,26 @@
 package org.acme.geometry;
 
 public class Coordinate {
-    
+    // Attributes
+    private double x;
+    private double y;
+
+    // Constructors
+    Coordinate(){
+        this.x = 0.0;
+        this.y = 0.0;
+    };
+    Coordinate(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
